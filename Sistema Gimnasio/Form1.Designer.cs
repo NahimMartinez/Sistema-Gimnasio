cbc648765e2ebd1f2b960fa0f70cf2588a06ad4b
Nahim <nahimmartinez03@gmail.com>
2025-08-28 10:15:33 -0300

    feature: add sidebar

--- Sistema Gimnasio/Form1.Designer.cs
+++ Sistema Gimnasio/Form1.Designer.cs
@@ -36,7 +36,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "HOLA";
+            this.Text = "Sistema de gestión de Gym";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Sistema Gimnasio/Form1.cs
+++ Sistema Gimnasio/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,69 @@ namespace Sistema_Gimnasio
 {
     public partial class Form1 : Form
     {
+
+        //Elementos sidebar, topbar, content
+        private Panel sidebar = new Panel();
+
         public Form1()
         {
             InitializeComponent();
+            BuildSideBar();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuildSideBar()
+        {
+            // Configure sidebar
+            sidebar.Dock = DockStyle.Left;
+            sidebar.Width = 150;
+            sidebar.BackColor = Color.FromArgb(41, 39, 40);
+            this.Controls.Add(sidebar);
+
+            var items = new (string id, string text)[]
+            {
+                ("dashboard", "Dashboard"),
+                ("usuarios", "Usuarios"),
+                ("socios", "Socios"),
+                ("clases", "Clases"),
+                ("proveedores", "Proveedores"),
+                ("membresias", "Membresias"),
+                ("reportes", "Reportes"),
+            };
+
+            // Crear botones sidebar
+            foreach (var item in items)
+            {
+                var btn = new Button
+                {
+                    Text = item.text,
+                    Name = item.id,
+                    Dock = DockStyle.Top,
+                    Height = 40,
+                    FlatStyle = FlatStyle.Flat,
+                    ForeColor = Color.White,
+                    BackColor = Color.FromArgb(41, 39, 40),
+                    FlatAppearance = { BorderSize = 0 },
+                    TextAlign = ContentAlignment.MiddleLeft,
+                    Padding = new Padding(20, 0, 0, 0)
+                };
+
+                //Agregar los botones al sidebar principal
+                sidebar.Controls.Add(btn);
+            }
+
+            // Agregar un panel que ocupe el espacio restante
+            var fillerPanel = new Panel
+            {
+                Dock = DockStyle.Fill,
+                BackColor = Color.FromArgb(41, 39, 40)
+            };
+            sidebar.Controls.Add(fillerPanel);
         }
     }
 }
