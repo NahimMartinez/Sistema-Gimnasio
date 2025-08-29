b5818b73b7c4d53a31767649d08b86dab60ca1e5
Dario <dariomartin3@gmail.com>
2025-08-29 17:46:43 -0300

    feat: add sidebar in disigner

--- Sistema Gimnasio/Form1.cs
+++ Sistema Gimnasio/Form1.cs
@@ -20,62 +20,9 @@ namespace Sistema_Gimnasio
         public Form1()
         {
             InitializeComponent();
-            BuildSideBar();
+            
 
-        }
+        }       
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void BuildSideBar()
-        {
-            // Configure sidebar
-            sidebar.Dock = DockStyle.Left;
-            sidebar.Width = 150;
-            sidebar.BackColor = Color.FromArgb(41, 39, 40);
-            this.Controls.Add(sidebar);
-
-            var items = new (string id, string text)[]
-            {
-                ("dashboard", "Dashboard"),
-                ("usuarios", "Usuarios"),
-                ("socios", "Socios"),
-                ("clases", "Clases"),
-                ("proveedores", "Proveedores"),
-                ("membresias", "Membresias"),
-                ("reportes", "Reportes"),
-            };
-
-            // Crear botones sidebar
-            foreach (var item in items)
-            {
-                var btn = new Button
-                {
-                    Text = item.text,
-                    Name = item.id,
-                    Dock = DockStyle.Top,
-                    Height = 40,
-                    FlatStyle = FlatStyle.Flat,
-                    ForeColor = Color.White,
-                    BackColor = Color.FromArgb(41, 39, 40),
-                    FlatAppearance = { BorderSize = 0 },
-                    TextAlign = ContentAlignment.MiddleLeft,
-                    Padding = new Padding(20, 0, 0, 0)
-                };
-
-                //Agregar los botones al sidebar principal
-                sidebar.Controls.Add(btn);
-            }
-
-            // Agregar un panel que ocupe el espacio restante
-            var fillerPanel = new Panel
-            {
-                Dock = DockStyle.Fill,
-                BackColor = Color.FromArgb(41, 39, 40)
-            };
-            sidebar.Controls.Add(fillerPanel);
-        }
     }
 }
