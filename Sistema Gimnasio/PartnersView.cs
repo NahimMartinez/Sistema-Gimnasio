982ed7074edcc08bbcbcb838c328cfed66baf75a
Dario <dariomartin3@gmail.com>
2025-08-31 21:07:54 -0300

    feat: add  view partner

---
+++ Sistema Gimnasio/PartnersView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Gimnasio
+{
+    public partial class PartnersView : UserControl
+    {
+        public PartnersView()
+        {
+            InitializeComponent();
+        }
+
+        private void LTiltePartne_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
