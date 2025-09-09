db7e8e95767e95ed6adfca9499cc834ee97ea7bd
Dario <dariomartin3@gmail.com>
2025-09-08 22:06:49 -0300

    feat(config): add database connection string and helper class

---
+++ Data/Connection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Data
+{
+    public class Connection
+    {
+        public static string chain = ConfigurationManager.ConnectionStrings["chain_conecction"].ConnectionString;
+    }
+}
