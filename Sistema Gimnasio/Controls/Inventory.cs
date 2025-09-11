0de6109f418e7ee8c63244a0a57a58b3c1f3c16a
Nahim Martinez <nahimmartinez03@gmail.com>
2025-09-11 12:52:05 -0300

    feature: add hard code data in inventory

--- Sistema Gimnasio/Controls/Inventory.cs
+++ Sistema Gimnasio/Controls/Inventory.cs
@@ -16,6 +16,7 @@ namespace Sistema_Gimnasio
         public Inventory()
         {
             InitializeComponent();
+            LoadFakeData();
         }
 
         private void BNewItem_Click(object sender, EventArgs e)
@@ -29,6 +30,16 @@ namespace Sistema_Gimnasio
 
                 }
             }
+        }
+
+        private void LoadFakeData()
+        {
+            //nombre, cantidad, fecha ingreso, categoria, estado
+            BoardInventory.Rows.Add("Mancuernas 10 kg", 2, "20/10/2025", "Mancuerna", "Activo");
+            BoardInventory.Rows.Add("Colchoneta", 5, "15/09/2025", "Colchoneta", "Activo");
+            BoardInventory.Rows.Add("Bicicleta Estatica", 1, "01/08/2025", "Cardio", "Inactivo");
+            BoardInventory.Rows.Add("Balon Medicinal 5 kg", 3, "10/11/2025", "Balon Medicinal", "Activo");
+            BoardInventory.Rows.Add("Cinta de Correr", 1, "05/07/2025", "Cardio", "Activo");
         }
     }
 }
