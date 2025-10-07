4a7d984edee421d6d45874dce9f2ef8eb147059a
Dario <dariomartin3@gmail.com>
2025-10-07 17:46:07 -0300

    feat: add entitie supplier

---
+++ Entities/Supplier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Supplier
+    {
+        public int IdProveedor { get; set; }   // PK autoincremental
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Cuit { get; set; }
+        public int Telefono { get; set; }
+        public string Email { get; set; }
+        public bool Estado { get; set; }     // true = activo, false = inactivo
+    }
+}
