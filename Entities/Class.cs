e3299d5ec2c9724a1febcfce38996d0341c4dfad
Nahim Martinez <nahimmartinez03@gmail.com>
2025-10-08 19:27:41 -0300

    feat: add entities class, classday, day, activity

---
+++ Entities/Class.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Class
+    {
+        public int IdClase { get; set; }
+        public int ActividadId { get; set; }
+        public int UsuarioId { get; set; }     // ID del Coach/Profesor
+        public TimeSpan HoraDesde { get; set; } // TimeSpan es el tipo para TIME de SQL
+        public TimeSpan HoraHasta { get; set; }
+        public decimal Precio { get; set; }
+        public int Cupo { get; set; }
+        public bool Estado { get; set; }
+
+        public List<Day> Dias { get; set; } // Lista para guardar los días en que se da la clase
+
+        public Class()
+        {
+            Dias = new List<Day>();
+        }
+    }
+}
