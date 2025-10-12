dd680ae562877bde9ccc0c0e1e466f38fb65f7c7
Nahim Martinez <nahimmartinez03@gmail.com>
2025-10-12 12:06:27 -0300

    feature: add entities Inventory and InventoryCategory

---
+++ Entities/Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    internal class Inventory
+    {
+        public int IdInventario { get; set; }
+        public int IdInventarioCategoria { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public bool Estado { get; set; }
+
+        public InventoryCategory InventarioCategoria { get; set; }
+    }
+}
