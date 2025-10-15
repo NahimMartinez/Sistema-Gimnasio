3f5eb328213291362d19484d716572c6d8c093e9
Dario <dariomartin3@gmail.com>
2025-10-15 08:22:03 -0300

    feat: add entitie membership and membershiptype

---
+++ Entities/Membership.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Membership
+    {
+        public int IdMembresia { get; set; }
+        public int IdSocio { get; set; }
+        public int IdTipo { get; set; }
+        public int IdClase { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ Entities/MembershipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class MembershipType
+    {
+        public int IdTipo { get; set; }
+        public string Nombre { get; set; }
+        public int Duracion { get; set; }
+    }
+}
